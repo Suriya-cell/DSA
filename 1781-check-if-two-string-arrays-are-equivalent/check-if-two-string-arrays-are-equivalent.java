4c9eb40e8fe1eec2eb2a43ae8c602fcf9087b311
Suriya N <suriyasuriya39904@gmail.com>
2026-09-11 14:08:37 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (23.72%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
@@ -1,16 +1,17 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        String sb = "";
-        String sb1 = "";
+        StringBuilder sb = new StringBuilder();
+        StringBuilder sb1 = new StringBuilder();
 
         for(String s : word1){
-            sb = sb + s;
-        }
-        for(String s : word2){
-            sb1 = sb1 + s;
+            sb.append(s);
         }
 
-        return sb.equals(sb1);
+        for(String s : word2){
+            sb1.append(s);
+        }
+
+        return sb.toString().equals(sb1.toString());
 
     }
 }
